87f172b55ac9c8b162adab079194bbd6cbab51cf
thiagofjv23 <contatothiagohenrique32@gmail.com>
2026-06-25 09:57:01 -0300

    Add additional fimPoints to the array

--- engine.js
+++ engine.js
@@ -1,4 +1,4 @@
-const fimPoints = [25, 20, 16, 13, 11, 10, 9, 8, 7, 6, 5, 4];
+const fimPoints = [25, 20, 16, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 const dnfReasons = [
     "Queda na Curva 1", 
     "Quebra de Motor", 
